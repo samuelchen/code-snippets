848000623df712abd7f73eb1e9efebf7ef160e05
Samuel Chen <samuelchen@users.noreply.github.com>
2020-02-28 22:10:40 +0800

    Update date_string.bat

--- bat/date_string.bat
+++ bat/date_string.bat
@@ -1,7 +1,7 @@
 @echo off
 rem set local ENABLEDELAYEDEXPANSION
 
-wmic /? >> null || echo "wmic not found." && echo 'quit'
+wmic /? >> nul|| echo "wmic not found." && echo 'quit'
 
 for /f %%i in ( 'wmic os get LocalDateTime /value' ) do (
     echo "%%i" | findstr "LocalDateTime" > nul && set DT=%%i
